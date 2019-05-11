22aaf4ca1327371c58dbeb69d8a8612cf523f021
mauricionr <mauricioskateboard@gmail.com>
2019-05-11 14:24:13 -0300

    about files

--- about_files.go
+++ about_files.go
@@ -1,11 +1,15 @@
 package go_koans
 
-import "io/ioutil"
-import "strings"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 func aboutFiles() {
 	filename := "about_files.go"
 	contents, _ := ioutil.ReadFile(filename)
 	lines := strings.Split(string(contents), "\n")
-	assert(lines[5] == __string__) // handling files is too trivial
+	fmt.Println(lines[5])
+	assert(lines[5] == "	\"strings\"") // handling files is too trivial
 }
